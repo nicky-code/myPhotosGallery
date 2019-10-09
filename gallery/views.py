d67014ecd6d5b7aa868f4837bc65a91a2ed38680
nicky-code <aline.nicole7@gmail.com>
2019-10-09 15:22:25 +0200

    first push

--- gallery/views.py
+++ gallery/views.py
@@ -3,5 +3,5 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to My Gallery')
+    return render(request, 'welcome.html')
 
